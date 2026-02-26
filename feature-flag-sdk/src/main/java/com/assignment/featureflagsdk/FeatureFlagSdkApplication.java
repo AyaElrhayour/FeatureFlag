0924c14d3ec51cd84c422fda875ad37d5be5e9bd
ayaelrhayour <Aya.Elrhayour@progressoft.com>
2026-02-26 22:04:29 +0000

    initializing project

---
+++ feature-flag-sdk/src/main/java/com/assignment/featureflagsdk/FeatureFlagSdkApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.featureflagsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagSdkApplication.class, args);
+	}
+
+}
